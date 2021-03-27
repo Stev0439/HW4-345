ac9c0672a7b22b96e8a619619b1ba0cded391a0f
Stev0439 <ssantiag@stevens.edu>
2021-03-27 19:19:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ async function listBranches(owner,repo)
 // 2. Write code to create a new repo
 async function createRepo(owner,repo)
 {
-	let options = getDefaultOptions("/user/repos", "POST");
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/generate`, "POST");
 	options.json = {
 		name: repo
 	};
